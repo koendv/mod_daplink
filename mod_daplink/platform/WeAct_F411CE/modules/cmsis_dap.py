1716f69d47e669c7722bf6a02453444b6798759e
Koen De Vleeschauwer <kdv@kdvelectronics.eu>
2023-04-29 09:52:03 +0200

    micropython v1.19

--- mod_daplink/platform/WeAct_F411CE/modules/cmsis_dap.py
+++ mod_daplink/platform/WeAct_F411CE/modules/cmsis_dap.py
@@ -2,8 +2,8 @@ def run():
     import pyb
     import daplink
     daplink.init()
-    app_request=bytearray(64)
-    app_response=bytearray(64)
+    app_request=bytes(64)
+    app_response=bytes(64)
     usb_hid=pyb.USB_HID()
     while(1):
         if (usb_hid.recv(app_request) and (daplink.process_request(app_request, app_response))[1] != 0):
